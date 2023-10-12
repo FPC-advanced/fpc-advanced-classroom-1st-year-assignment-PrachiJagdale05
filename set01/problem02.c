de202e4dd1b1cf9736ed139c6781186a344123f6
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-10-12 16:19:38 +0530

    committed 10/12/2023, 4:19:36 PM

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,16 @@
 //2. Write a C program to add two numbers.
+#include<stdio.h>
+int main (){
+    int a, b,c;
+    printf("enter 1 number:");
+    scanf("%d",&a);
+    printf("enter 2 number:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("the sum of%d and %d is %d", a,b,c);
+    return 0;
+    
+}
+ 
+
 
