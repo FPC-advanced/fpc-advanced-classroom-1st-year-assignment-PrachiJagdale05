cec030d637daa8ba4ef6a67c147a436147aa53db
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-10-18 15:22:40 +0530

    committed 10/18/2023, 3:22:40 PM

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,1 +1,23 @@
 //Write a C program to add two numbers using **pass by reference**
+#include<stdio.h>
+void input(int *a, int *b){
+    printf("enter the first number:");
+    scanf("%d",a);
+    printf("enter the second number:");
+    scanf("%d",b);
+
+}
+void add(int a, int b, int*sum){
+    *sum=a+b;
+}
+void output(int a,int b, int sum){
+    printf("the sum of %d and %d is %d\n",a,b,sum);
+}
+int main(){
+    int num1,num2,result;
+    input(&num1,&num2);
+    add(num1,num2,&result);
+    output(num1,num2,result);
+    return 0;
+
+}
