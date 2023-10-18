de5b11d2235127ae0e0a323275bb780453f26578
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-10-18 15:46:38 +0530

    committed 10/18/2023, 3:46:37 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,2 +1,31 @@
 // Write a C program to compare three numbers using **pass by value**
+#include<stdio.h>
+int input(){
+    int num;
+    printf("enter a number:");
+    scanf("%d",&num);
+    return num;
+}
+int compare(int a,int b, int c){
+    if (a>=b && a>=c){
+        return a;
+    }else if(b>=a && b>=c){
+        return b;
+    }else{
+        return c;
+    }
+}
+void output(int a,int b,int c,int largest){
+    printf("the largest number among %,%dand %d is%d",a,b,c,largest);
+}
+int main(){
+    int num1,num2,num3,largest;
+    num1=input();
+    num2=input();
+    num3=input();
+    largest = compare(num1,num2,num3);
+    output(num1, num2,num3,largest);
+    return 0;
+}
+
 
--- set01/problem07.c
+++ set01/problem07.c
@@ -1,1 +1,7 @@
 // Write a C program to find sum of all natural numbers until _n_
+int input(){
+    int num;
+    printf("enter a number:");
+    scanf("%d",&num);
+    return num;
+}
