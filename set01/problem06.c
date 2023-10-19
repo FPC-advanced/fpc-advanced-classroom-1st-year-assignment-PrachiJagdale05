66b2436890ce1e21b21b2e113b18fdfac3583bc7
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-10-19 15:58:57 +0530

    committed 10/19/2023, 3:58:56 PM

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,1 +1,20 @@
-//Write a C program to compare three numbers using **pass by reference**
+//Write a C program to compare three numbers using **pass by reference*
+#include<stdio.h>
+int input(int *a,int *b,int *c){
+    
+    printf("enter the first number:");
+    scanf("%d",a);
+    printf("enter the second number:");
+    scanf("%d",b);
+    printf("enter the third number:");
+    scanf("%d",c);
+}
+ void comapre(int a,int b, int c,int *largest){
+    if(a>=b && a>=c){
+        *largest=a;
+    } else if(b>=a && b>=c){
+        *largest
+    }
+    }
+ }
+
