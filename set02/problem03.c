7e90f1e0224928a1ba029bc84a8655737f202e15
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-10-29 23:12:09 +0530

    committed 10/29/2023, 11:12:08 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -1,3 +1,21 @@
 //Write a program find whether a number is a composite number
 #include<stdio.h>
-int input
+int input_number() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    return n;
+}
+int is_composite(int n) {
+    if (n <= 1) {
+        return 0; // 0 and 1 are not composite numbers
+    }
+    
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            return 1; // Found a factor other than 1 and n, so it's composite
+        }
+    }
+    
+    return 0; // No factors other than 1 and n, so it's not composite
+}
