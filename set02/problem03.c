c9c92d9c00b4bb393ae84c1069b1b20179fe9acb
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-10-29 23:16:16 +0530

    committed 10/29/2023, 11:16:15 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -6,16 +6,29 @@ int input_number() {
     scanf("%d", &n);
     return n;
 }
-int is_composite(int n) {
+int is_composite(int n){
     if (n <= 1) {
-        return 0; // 0 and 1 are not composite numbers
+        return 0; 
     }
     
     for (int i = 2; i <= n / 2; i++) {
         if (n % i == 0) {
-            return 1; // Found a factor other than 1 and n, so it's composite
-        }
+            return 1; 
     }
-    
-    return 0; // No factors other than 1 and n, so it's not composite
+    return 0; 
+}
+}
+void output(int n, int result) {
+    if (result) {
+        printf("%d is a composite number.\n", n);
+    } else {
+        printf("%d is not a composite number.\n", n);
+    }
+}
+
+int main() {
+    int number = input_number();
+    int result = is_composite(number);
+    output(number, result);
+    return 0;
 }
