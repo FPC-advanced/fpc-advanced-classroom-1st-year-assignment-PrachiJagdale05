f65cedbf9ba997eb715cfde347ceb7419a0d0c16
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-11-07 11:52:56 +0530

    committed 11/7/2023, 11:52:55 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -24,4 +24,30 @@ float distance = find_distance(x1, y1, x2, y2);
 output(x1, y1, x2, y2, distance);
 return 0;
 
+}// Write a program to find the distance between 2 points
+#include<stdio.h>
+
+void input(float *x1, float *y1, float *x2, float *y2){
+    printf("enter the values for(x1,y1):");
+    scanf("%f,%f",x1,y1);
+    printf("enter the values for(x2,y2):");
+    scanf("%f,%f",x2,y2);
+}
+float find_distance(float x1, float y1, float x2, float y2){
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    return dx * dx + dy * dy; 
+}
+void output(float x1, float y1, float x2, float y2, float distance){
+     printf("The distance between Point  (%f, %f) and Point (%f, %f) is: %f\n", x1, y1, x2, y2, distance);
+}
+
+int main(){
+    
+float x1, y1, x2, y2;
+input(&x1, &y1, &x2, &y2);    
+float distance = find_distance(x1, y1, x2, y2);
+output(x1, y1, x2, y2, distance);
+return 0;
+
 }
