7b19808daa3966da50d8c9e931189b3aa9bff799
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-11-07 11:59:56 +0530

    committed 11/7/2023, 11:59:55 AM

--- set03/problem03.c
+++ set03/problem03.c
@@ -6,6 +6,23 @@ int input_number(){
     scanf("%d",&n);
     return n;
 }
-int is_prime(int n){
-    i
+int is_prime(int n) {
+    if (n <= 1){
+
+    }
+         
+
+    for (int i = 2; i <= n/2; i++) {
+        if (n % i == 0)
+            return 0; 
+    }
+
+    return 1; 
+}
+
+void output(int n, int result) {
+    if (result)
+        printf("%d is a prime number.\n", n);
+    else
+        printf("%d is not a prime number.\n", n);
 }
