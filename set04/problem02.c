d375e99f396bce43ff2b27dfb6ac07cb970d6968
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-11-15 23:48:06 +0530

    committed 11/15/2023, 11:48:03 PM

--- set04/problem02.c
+++ set04/problem02.c
@@ -17,13 +17,9 @@ Fraction input_fraction() {
     scanf("%d", &frac.den);
     return frac;
 }
-
-// Function to find the smallest fraction
 Fraction find_smallest(Fraction frac1, Fraction frac2, Fraction frac3) {
-    // Convert each fraction to a common denominator for comparison
     int common_den = frac1.den * frac2.den * frac3.den;
 
-    // Normalize fractions to the common denominator
     frac1.num *= common_den / frac1.den;
     frac1.den = common_den;
 
@@ -33,7 +29,6 @@ Fraction find_smallest(Fraction frac1, Fraction frac2, Fraction frac3) {
     frac3.num *= common_den / frac3.den;
     frac3.den = common_den;
 
-    // Find the smallest fraction
     if ((frac1.num * frac2.den) < (frac2.num * frac1.den) && (frac1.num * frac3.den) < (frac3.num * frac1.den)) {
         return frac1;
     } else if ((frac2.num * frac3.den) < (frac3.num * frac2.den)) {
@@ -43,7 +38,6 @@ Fraction find_smallest(Fraction frac1, Fraction frac2, Fraction frac3) {
     }
 }
 
-// Function to output the result
 void output(Fraction smallest) {
     printf("The smallest fraction is %d/%d\n", smallest.num, smallest.den);
 }
