9347a0a0258c8ae086a18775bae813e20cc5419b
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-11-28 10:12:40 +0530

    committed 11/28/2023, 10:12:39 AM

---
+++ set05/problem02.c
@@ -0,0 +1,37 @@
+//Write a program to find the weight of a camel, given height, length and stomach radius using four functions.
+#include <stdio.h>
+#include <math.h>
+
+void input_camel_details(float *radius, float *height, float *length);
+float find_weight(float radius, float height, float length);
+void output(float radius, float height, float length, float weight);
+
+
+void input_camel_details(float *radius, float *height, float *length) {
+    printf("Enter stomach radius of the camel: ");
+    scanf("%f", radius);
+    printf("Enter height of the camel: ");
+    scanf("%f", height);
+    printf("Enter length of the camel: ");
+    scanf("%f", length);
+}
+
+float find_weight(float radius, float height, float length) {
+    return 3.14159265359 * pow(radius, 3) * sqrt(height * length);
+}
+
+void output(float radius, float height, float length, float weight) {
+    printf("Stomach Radius: %.2f\n", radius);
+    printf("Height: %.2f\n", height);
+    printf("Length: %.2f\n", length);
+    printf("Weight of the camel: %.2f\n", weight);
+}
+int main() {
+    float stomach_radius, camel_height, camel_length, camel_weight;
+
+    input_camel_details(&stomach_radius, &camel_height, &camel_length);
+    camel_weight = find_weight(stomach_radius, camel_height, camel_length);
+    output(stomach_radius, camel_height, camel_length, camel_weight);
+
+    return 0;
+}
