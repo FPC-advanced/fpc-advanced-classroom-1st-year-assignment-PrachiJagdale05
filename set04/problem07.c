aaaa6150ac2be2ac4ddb1d258e95a6e044909791
PrachiJagdale05 <jagdalep.bsc23@rvu.edu.in>
2023-11-28 09:55:39 +0530

    committed 11/28/2023, 9:55:37 AM

--- set04/problem07.c
+++ set04/problem07.c
@@ -8,7 +8,8 @@ typedef struct {
 Fraction input_fraction();
 int find_gcd(int a, int b);
 Fraction add_fractions(Fraction f1, Fraction f2);
-void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum);
+int  output(Fraction f1, Fraction f2, Fraction f3, Fraction sum);
+
 
 int main() {
     Fraction fraction1, fraction2, result;
